2855d26d74c905b771156e2b464678d009d75426
Gongback <94734506+newkincode@users.noreply.github.com>
2022-07-23 15:10:08 +0900

    2.1

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace tokenShield
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("error: The program did not start");
+        }
     }
 }
